1667789331a9ced0295fd8b8f5614c21a2aa52e7
xiaopacychen <104703591+xiaopacychen@users.noreply.github.com>
2023-03-27 02:05:09 +0800

    Add files via upload

---
+++ change.js
@@ -0,0 +1,11 @@
+var time=new Date();
+var year=time.getFullYear();
+var month=time.getMonth();
+var date=time.getDate();
+var hour=time.getHours();
+var minute=time.getMinutes();
+var start=[]
+//var second=time.getSeconds();
+
+document.getElementById("data-startdate").innerHTML = "已开站<b style=\"font-size:20px;\">我也不知道几<\/b>天";
+
